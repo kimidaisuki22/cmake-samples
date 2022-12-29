0e7e7357c8edbe4501c097a80b08ad05f7398275
kimdaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2022-12-29 07:27:36 -0500

    use this lib in actual.

--- head-only-lib/main.cpp
+++ head-only-lib/main.cpp
@@ -3,7 +3,9 @@
 void say_hello(){
     std::cout << "Hello, from head-only-lib!\n";
 }
-// #include <head_onlys/utils.hpp>
+#include <head_onlys/utils.hpp>
 int main(){
     say_hello();
+
+    std::cout << min(1,2,3,4,5,56) <<"\n";
 }
